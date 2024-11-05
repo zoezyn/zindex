ed40f6ddb8dcca9b8d54611cff90c312c52f015f
Zengyu-Yan <zoe.yan@porsche.digital>
2024-11-05 22:18:06 +0100

    test1

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base:"./",
+  plugins: [react()],
   server: {
     port: 3000,
     // Optional: Automatically open browser on server start
