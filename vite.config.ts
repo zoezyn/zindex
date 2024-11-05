fcf1911e145ce4262cfb4f7fff9760b49b1a93f4
Zengyu-Yan <zoe.yan@porsche.digital>
2024-11-05 23:22:46 +0100

    clean

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/",
+  // base: '',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  }
+  server: {
+    port: 3000,
+    // Optional: Automatically open browser on server start
+    open: true,
+    // Optional: If port 3000 is taken, don't try another port
+    strictPort: true
+  },
+  // build: {
+  //   outDir: 'dist',
+  //   rollupOptions: {
+  //     input: {
+  //       main: './index.html'
+  //     }
+  //   }
+  // }
 })
