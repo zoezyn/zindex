cbd6a5c2cc2741563206e38a6a35c1fe15433c16
Zengyu-Yan <zoe.yan@porsche.digital>
2025-01-10 09:46:12 +0100

    fix bug

--- src/pages/SuccessPage/SuccessPage.tsx
+++ src/pages/SuccessPage/SuccessPage.tsx
@@ -1,6 +1,5 @@
 import { supabase } from '../../lib/supabaseClient'
 import './SuccessPage.css'
-import { useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { User } from '@supabase/supabase-js'
 import BookCard from '../../components/bookcard/bookcard'
